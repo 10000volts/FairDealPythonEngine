4aacbb98ca5f5f5c92123d22b6cbfa407ff15788
10000volts <602908228@qq.com>
2020-05-23 23:10:47 +0800

    update

--- core/output.py
+++ core/output.py
@@ -1,12 +1,13 @@
 import socket
+from utils.hints import Hint
 
 
 def send_2_socket(acceptor, msg):
     pass
 
 
-def send_msg(acceptor, msg):
-    pass
+def send_msg(acceptor, msg: dict):
+    print(Hint.hints[int(msg['op'])].format(msg['result']))
 
 
 def send_2_ai(acceptor, msg):
--- models/player.py
+++ models/player.py
@@ -1,6 +1,6 @@
 class Player:
     def __init__(self, name, name_in_game, op_method, ipt):
         self.name = name
-        self.name_in_game = name
+        self.name_in_game = name_in_game
         self.op_method = op_method
         self.terminal = ipt
