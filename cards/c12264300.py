b6260565ae73955a40486064a96da31f778e208a
10000volts <602908228@qq.com>
2020-09-07 14:31:24 +0800

    fix

--- cards/c12264300.py
+++ cards/c12264300.py
@@ -3,6 +3,26 @@ from utils.constants import EEffectDesc, ETimePoint, ELocation, ECardType
 from utils.common_effects import EffTriggerCostMixin, EffCommonStrategy
 
 
+class E4(EffTriggerCostMixin):
+    """
+    离场时更新。
+    """
+    def __init__(self, c):
+        super().__init__(desc=EEffectDesc.EFFECT_END, host=c, trigger=True, force=True,
+                         passive=True)
+
+    def condition(self, tp):
+        if tp.tp == ETimePoint.OUT_FIELD_END:
+            return tp.args[0] is self.host
+        return False
+
+    def execute(self):
+        p = self.game.get_player(self.host)
+        for c in p.on_field:
+            if c is not None and c.type == ECardType.EMPLOYEE and len(c.effects) == 0:
+                c.ATK.update()
+
+
 class E3(EffTriggerCostMixin):
     """
     更新攻击力。
@@ -64,3 +84,4 @@ def give(c):
     c.register_effect(E1(c))
     c.register_effect(E2(c))
     c.register_effect(E3(c))
+    c.register_effect(E4(c))
