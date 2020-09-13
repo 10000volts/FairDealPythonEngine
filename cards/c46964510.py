5e8d2c5ca02db91fa0dac9e0df3756555a9ed216
10000volts <602908228@qq.com>
2020-09-13 19:03:04 +0800

    Update c46964510.py

--- cards/c46964510.py
+++ cards/c46964510.py
@@ -65,42 +65,42 @@ class E2(EffTriggerCostMixin):
         self.reacted.pop().args[-1] = 0
 
 
-class E1(EffTriggerCostMixin):
-    """
-    入场：盖放1策略。
-    """
-    def __init__(self, host):
-        super().__init__(desc=EEffectDesc.SET_CARD, host=host, trigger=True)
-
-    def condition(self, tp):
-        if tp.tp == ETimePoint.SUCC_SUMMON:
-            if tp.args[0] is self.host:
-                p = self.game.get_player(self.host)
-                for pos in range(3, 6):
-                    if p.on_field[pos] is None:
-                        for c in p.hand:
-                            if c.type == ECardType.STRATEGY:
-                                tp = TimePoint(ETimePoint.TRY_SET_STRATEGY, self, [c, 1])
-                                self.game.enter_time_point(tp)
-                                if tp.args[-1]:
-                                    return True
-        return False
-
-    def execute(self):
-        """
-        执行效果。触发式效果获得当前时点信息时请使用reacted.pop()。
-        调用基类方法进行输出。
-        :return:
-        """
-        def check(c):
-            return (c.location == ELocation.HAND + 2 - p.sp) & (c.type == ECardType.STRATEGY)
-        p = self.game.get_player(self.host)
-        tgt = self.game.choose_target(p, p, check, self)
-        if tgt is not None:
-            self.game.set_strategy(p, p, tgt, self)
-            e2 = E2(tgt)
-            tgt.register_effect(e2)
-            tgt.register_effect(EffUntil(tgt, e2, lambda tp: tp.tp == ETimePoint.TURN_END))
+# class E1(EffTriggerCostMixin):
+#     """
+#     入场：盖放1策略。
+#     """
+#     def __init__(self, host):
+#         super().__init__(desc=EEffectDesc.SET_CARD, host=host, trigger=True)
+#
+#     def condition(self, tp):
+#         if tp.tp == ETimePoint.SUCC_SUMMON:
+#             if tp.args[0] is self.host:
+#                 p = self.game.get_player(self.host)
+#                 for pos in range(3, 6):
+#                     if p.on_field[pos] is None:
+#                         for c in p.hand:
+#                             if c.type == ECardType.STRATEGY:
+#                                 tp = TimePoint(ETimePoint.TRY_SET_STRATEGY, self, [c, 1])
+#                                 self.game.enter_time_point(tp)
+#                                 if tp.args[-1]:
+#                                     return True
+#         return False
+#
+#     def execute(self):
+#         """
+#         执行效果。触发式效果获得当前时点信息时请使用reacted.pop()。
+#         调用基类方法进行输出。
+#         :return:
+#         """
+#         def check(c):
+#             return (c.location == ELocation.HAND + 2 - p.sp) & (c.type == ECardType.STRATEGY)
+#         p = self.game.get_player(self.host)
+#         tgt = self.game.choose_target(p, p, check, self)
+#         if tgt is not None:
+#             self.game.set_strategy(p, p, tgt, self)
+#             e2 = E2(tgt)
+#             tgt.register_effect(e2)
+#             tgt.register_effect(EffUntil(tgt, e2, lambda tp: tp.tp == ETimePoint.TURN_END))
 
 
 def give(c):
@@ -109,6 +109,5 @@ def give(c):
     :param c:
     :return:
     """
-    c.register_effect(E1(c))
     c.register_effect(E3(c))
     c.register_effect(E4(c))
