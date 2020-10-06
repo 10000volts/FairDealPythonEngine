e1ec70ab36aee184c29fa0b2fc1a3e2e8313feb7
10000volts <602908228@qq.com>
2020-10-06 19:47:47 +0800

    Update c87938220.py

--- cards/c87938220.py
+++ cards/c87938220.py
@@ -6,34 +6,31 @@ from utils.constants import EEffectDesc, ETimePoint, ELocation
 
 class E1(Effect):
     def __init__(self, host):
-        super().__init__(host=host, desc=EEffectDesc.SPECIAL_SUMMON, ef_id='879382200')
+        super().__init__(host=host, desc=EEffectDesc.HEAL, trigger=True, force=True)
 
     def condition(self, tp):
-        if tp.tp == ETimePoint.ASK4EFFECT:
+        if tp.tp == ETimePoint.TURN_BEGIN:
             p = self.game.get_player(self.host)
-            return (self.host.location == ELocation.HAND + 2 - p.sp) & (self.ef_id not in p.ef_limiter)
+            return (self.host.location == ELocation.HAND + 2 - p.sp) & (self.ef_id not in p.ef_limiter) & \
+                   (self.game.turn_player is p)
         return False
 
     def cost(self, tp):
-        if self.condition(tp):
-            p = self.game.get_player(self.host)
-            self.game.show_card(p, self.host.vid, self)
-
-            if self.ef_id in self.game.ef_listener:
-                p.ef_limiter[self.ef_id] += 1
-            else:
-                p.ef_limiter[self.ef_id] = 1
-            return True
+        if tp not in self.reacted:
+            self.reacted.append(tp)
+            if self.condition(tp):
+                self.game.show_card(self.game.get_player(self.host), self.host.vid, self)
+                return True
         return False
 
     def execute(self):
         p = self.game.get_player(self.host)
-        p.leader.effects[0].scr_arg += self.host.ATK.value
+        p.leader.effects[0].scr_arg += self.host.ATK.value / 2
 
 
 class E2(EffSummon):
     def __init__(self, host):
-        super().__init__(desc=EEffectDesc.DEAL_DAMAGE, host=host)
+        super().__init__(desc=EEffectDesc.VICTORY, host=host)
 
     def condition(self, tp):
         if super().condition(tp):
